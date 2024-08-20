731564c01a204cca29bdfac91efda69775137a79
Satyendra-TA <satyendra.mishra@tigeranalytics.com>
2024-08-20 17:54:38 +0530

    add preprocessing pipelines to ingest data

--- src/house_price_predictor/ingest_data.py
+++ src/house_price_predictor/ingest_data.py
@@ -2,10 +2,21 @@ import os
 import tarfile
 from argparse import ArgumentParser
 
+import numpy as np
 import pandas as pd
+import scipy
+from joblib import Parallel, delayed
 from six.moves import urllib
+from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.impute import SimpleImputer
 from sklearn.model_selection import train_test_split
+from sklearn.pipeline import (
+    FeatureUnion,
+    Pipeline,
+    _fit_transform_one,
+    _transform_one,
+)
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
 
 DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml/master/"
 HOUSING_URL = DOWNLOAD_ROOT + "datasets/housing/housing.tgz"
@@ -57,31 +68,247 @@ def load_housing_data(housing_path: str = HOUSING_PATH) -> pd.DataFrame:
     return pd.read_csv(csv_path)
 
 
-def engineer_features(housing: pd.DataFrame) -> pd.DataFrame:
+def get_column_indices(df, col_names):
     """
-    Feature engineering for housing data
+    finds the indices of column names in the dataframe's list of columns
+
+    Parameter
+    ---------
+    df : pd.DataFrame
+        input dataframe
+    col_names: list[str]
+        list of column names whoswe indices are to be determined
+
+    """
+    return [df.columns.get_loc(c) for c in col_names]
+
+
+class CombinedAttributesAdder(
+    BaseEstimator, TransformerMixin, auto_wrap_output_keys=None
+):
+    """Adds attributes to the input dataset
 
     Parameters
     ----------
-    housing : pd.DataFrame
-        a pandas dataframe
+    add_bedroom_per_room : bool
+        whether to add bedrooms per room
+    idx : list[int]
+        indexes of columns for feature engineering
 
-    Returns
+    Methods
     -------
-    housing : pd. DataFrame
-        pandas dataframe with engineered features
+    fit(X, y=None)
+        fits the model on the dataset X
+    transform(X)
+        transforms X based on statistics learned in fit()
 
     """
-    housing["rooms_per_household"] = (
-        housing["total_rooms"] / housing["households"]
+
+    def __init__(self, idx, add_bedrooms_per_room=True):  # no *args or **kargs
+        self.add_bedrooms_per_room = add_bedrooms_per_room
+        self.idx = idx
+
+    def fit(self, X, y=None):
+        return self  # nothing else to do
+
+    def transform(self, X):
+        columns = X.columns.tolist()
+        X = X.to_numpy()
+        rooms_ix, bedrooms_ix, population_ix, households_ix = self.idx
+        rooms_per_household = X[:, rooms_ix] / X[:, households_ix]
+        population_per_household = X[:, population_ix] / X[:, households_ix]
+        if self.add_bedrooms_per_room:
+            columns += [
+                "rooms_per_household",
+                "population_per_household",
+                "bedrooms_per_room",
+            ]
+            bedrooms_per_room = X[:, bedrooms_ix] / X[:, rooms_ix]
+            modified_df = pd.DataFrame(
+                np.c_[
+                    X,
+                    rooms_per_household,
+                    population_per_household,
+                    bedrooms_per_room,
+                ],
+                columns=columns,
+            )
+            return modified_df
+        else:
+            columns += ["rooms_per_household", "population_per_household"]
+            modified_df = pd.DataFrame(
+                np.c_[
+                    X,
+                    rooms_per_household,
+                    population_per_household,
+                ],
+                columns=columns,
+            )
+            return modified_df
+
+
+class CustomSimpleImputer(BaseEstimator, TransformerMixin):
+    def __init__(self, variables, strategy):
+        self.variables = variables
+        self.strategy = strategy
+        self.imp = SimpleImputer(missing_values=np.nan, strategy=self.strategy)
+
+    def fit(self, X, y=None):
+        X_ = X.loc[:, self.variables]
+        self.imp.fit(X_)
+        return self
+
+    def transform(self, X):
+        X_ = X.loc[:, self.variables]
+        X_transformed = pd.DataFrame(
+            self.imp.transform(X_), columns=self.variables
+        )
+        X.drop(self.variables, axis=1, inplace=True)
+        X[self.variables] = X_transformed[self.variables].values
+        return X
+
+
+class CustomStandardScaler(BaseEstimator, TransformerMixin):
+    def __init__(self, variables):
+        self.variables = variables
+        self.scaler = StandardScaler()
+
+    def fit(self, X, y=None):
+        X_ = X.loc[:, self.variables]
+        self.scaler.fit(X_)
+        return self
+
+    def transform(self, X):
+        X_ = X.loc[:, self.variables]
+        # get one-hot encoded feature in df format
+        X_transformed = pd.DataFrame(
+            self.scaler.transform(X_),
+            columns=self.scaler.get_feature_names_out(),
+        )
+
+        # Remove columns that are one hot encoded in original df
+        X.drop(self.variables, axis=1, inplace=True)
+
+        # Add one hot encoded feature to original df
+        X[self.scaler.get_feature_names_out()] = X_transformed[
+            self.scaler.get_feature_names_out()
+        ].values
+        return X
+
+
+class CustomOneHotEncoder(BaseEstimator, TransformerMixin):
+    def __init__(self, variables, sparse_output=True):
+        self.variables = variables
+        self.sparse_output = sparse_output
+        self.ohe = OneHotEncoder(
+            drop="first",
+            sparse_output=self.sparse_output,
+            handle_unknown="ignore",
+        )
+
+    def fit(self, X, y=None):
+        X_ = X.loc[:, self.variables]
+        self.ohe.fit(X_)
+        return self
+
+    def transform(self, X):
+        X_ = X.loc[:, self.variables]
+        # get one-hot encoded feature in df format
+        array = (
+            self.ohe.transform(X_).toarray()
+            if self.sparse_output
+            else self.ohe.transform(X_)
+        )
+        X_transformed = pd.DataFrame(
+            array,
+            columns=self.ohe.get_feature_names_out(),
+        )
+
+        # Remove columns that are one hot encoded in original df
+        X.drop(self.variables, axis=1, inplace=True)
+
+        # Add one hot encoded feature to original df
+        X[self.ohe.get_feature_names_out()] = X_transformed[
+            self.ohe.get_feature_names_out()
+        ].values
+        return X
+
+
+class PandasFeatureUnion(FeatureUnion):
+    def fit_transform(self, X, y=None, **fit_params):
+        self._validate_transformers()
+        result = Parallel(n_jobs=self.n_jobs)(
+            delayed(_fit_transform_one)(
+                transformer=trans, X=X, y=y, weight=weight, **fit_params
+            )
+            for name, trans, weight in self._iter()
+        )
+        if not result:
+            # All transformers are None
+            return np.zeros((X.shape[0], 0))
+        Xs, transformers = zip(*result)
+        # print(Xs[1])
+        self._update_transformer_list(transformers)
+        if any(scipy.sparse.issparse(f) for f in Xs):
+            Xs = scipy.sparse.hstack(Xs).tocsr()
+        else:
+            Xs = self.merge_dataframes_by_column(Xs)
+        return Xs
+
+    def merge_dataframes_by_column(self, Xs):
+        concatenated = pd.concat(Xs, axis="columns", copy=False)
+        return concatenated
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        Xs = Parallel(n_jobs=self.n_jobs)(
+            delayed(_transform_one)(
+                transformer=trans, X=X, y=None, weight=weight
+            )
+            for name, trans, weight in self._iter()
+        )
+        if not Xs:
+            # All transformers are None
+            return np.zeros((X.shape[0], 0))
+        if any(scipy.sparse.issparse(f) for f in Xs):
+            Xs = scipy.sparse.hstack(Xs).tocsr()
+        else:
+            Xs = self.merge_dataframes_by_column(Xs)
+        return Xs
+
+
+def get_preprocessing_pipeline(df, num_attributes, cat_attributes):
+    df = df.copy()
+    # column indices for feature enfinnering
+    col_names = ["total_rooms", "total_bedrooms", "population", "households"]
+    indices = get_column_indices(df, col_names)
+
+    # pipeline for numerical attributes
+    num_pipeline = Pipeline(
+        [
+            (
+                "imputer",
+                CustomSimpleImputer(num_attributes, strategy="median"),
+            ),
+            (
+                "attribs_adder",
+                CombinedAttributesAdder(indices, add_bedrooms_per_room=False),
+            ),
+            ("std_scaler", CustomStandardScaler(num_attributes)),
+        ]
     )
-    housing["bedrooms_per_room"] = (
-        housing["total_bedrooms"] / housing["total_rooms"]
+
+    # pipeline for categorical variables
+    cat_pipeline = Pipeline([("OHE", CustomOneHotEncoder(cat_attributes))])
+
+    # full pipeline
+    full_pipeline = PandasFeatureUnion(
+        [("numerical_pipe", num_pipeline), ("categorical_pipe", cat_pipeline)]
     )
-    housing["population_per_household"] = (
-        housing["population"] / housing["households"]
-    )
-    return housing
+
+    return full_pipeline
 
 
 def split_data(dataset, test_size):
@@ -105,8 +332,10 @@ def split_data(dataset, test_size):
 
     """
     train, test = train_test_split(
-        housing_data, test_size=0.2, random_state=42
+        dataset, test_size=test_size, random_state=42
     )
+    train = train.reset_index(drop=True)
+    test = test.reset_index(drop=True)
     return train, test
 
 
@@ -126,54 +355,49 @@ if __name__ == "__main__":
 
     if not os.path.exists(args.output_path):
         raise "The specified path %s does not exist" % (args.output_path)
-
+    # load the data
     housing_data = load_housing_data()
-
+    target = ["median_house_value"]
+    # train test split
     train_set, test_set = split_data(housing_data, test_size=0.2)
 
+    # Features dataframe
     housing_df = train_set.drop(
         "median_house_value", axis=1
     )  # drop labels for training set
 
+    # training labels
     housing_labels = train_set["median_house_value"].copy()
 
+    # numerical features
     housing_num = housing_df.drop("ocean_proximity", axis=1)
 
-    imputer = SimpleImputer(strategy="median")
-    imputer.fit(housing_num)
-    X = imputer.transform(housing_num)
+    # preprocessing pipeline
+    num_attribs = housing_num.columns.tolist()
 
-    housing_tr = pd.DataFrame(
-        X, columns=housing_num.columns, index=housing_df.index
+    cat_attribs = ["ocean_proximity"]
+
+    preprocessing_pipeline = get_preprocessing_pipeline(
+        housing_df, num_attribs, cat_attribs
     )
+    housing_prepared = preprocessing_pipeline.fit_transform(housing_df)
 
-    housing_tr = engineer_features(housing_tr)
-
-    housing_cat = housing_df[["ocean_proximity"]]
-    housing_prepared = housing_tr.join(
-        pd.get_dummies(housing_cat, drop_first=True)
-    )
-
+    # preprocess the test set
     X_test = test_set.drop("median_house_value", axis=1)
     y_test = test_set["median_house_value"].copy()
 
-    X_test_num = X_test.drop("ocean_proximity", axis=1)
-    X_test_prepared = imputer.transform(X_test_num)
-    X_test_prepared = pd.DataFrame(
-        X_test_prepared, columns=X_test_num.columns, index=X_test.index
+    X_test_prepared = preprocessing_pipeline.transform(X_test)
+
+    # save the feature and label files as csv
+    housing_prepared.to_csv(
+        os.path.join(args.output_path, "train_X.csv"), index=None
     )
-
-    X_test_prepared = engineer_features(X_test_prepared)
-
-    X_test_cat = X_test[["ocean_proximity"]]
-    X_test_prepared = X_test_prepared.join(
-        pd.get_dummies(X_test_cat, drop_first=True)
+    housing_labels.to_csv(
+        os.path.join(args.output_path, "train_labels.csv"), index=None
     )
-
-    housing_tr["median_house_value"] = housing_labels.to_numpy()
-    X_test_prepared["median_house_value"] = y_test.to_numpy()
-
-    housing_tr.to_csv(os.path.join(args.output_path, "train.csv"), index=False)
     X_test_prepared.to_csv(
-        os.path.join(args.output_path, "test.csv"), index=False
+        os.path.join(args.output_path, "test_X.csv"), index=None
+    )
+    y_test.to_csv(
+        os.path.join(args.output_path, "test_labels.csv"), index=None
     )
